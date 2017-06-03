c555b08f232d10bbbe919ad307aced0cfcd177e7
陆文 <993762194@qq..com>
2017-06-03 17:35:09 +0800

    尝试微信分享接口

--- assets/js/index.js
+++ assets/js/index.js
@@ -198,6 +198,7 @@ void function() {
   $('.testAgain').on('click', function() {
     // localStorage.removeItem('result');
     $('.result-btn-wrap').removeClass('show');
+    $('.test-btn').removeClass('hide');
     $('.test-cloud').removeClass('show moveCloud');
     $('.cloud-background').removeClass('show moveCloud');
     $('.result').removeClass('show result' + result + ' moveResult' + result);
@@ -209,15 +210,45 @@ void function() {
   $('.share-tip').on('click', function() {
     $('.share-tip').removeClass('show');
   });
-  // wx.config({
-  //   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-  //   appId: '', // 必填，公众号的唯一标识
-  //   timestamp: '', // 必填，生成签名的时间戳
-  //   nonceStr: '', // 必填，生成签名的随机串
-  //   signature: '',// 必填，签名，见附录1
-  //   jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-  // });
+  console.log(location.href);
+  $.ajax({
+    type: 'post',
+    url: 'http://192.168.0.53:2222/getsignature',
+    data: {
+        url: location.href.split('#')[0]
+    },
+    success: function(r) {
+      r = JSON.parse(r);
+      wx.config({
+        debug: true,
+        appId: r.appId,
+        timestamp: r.timestamp,
+        nonceStr: r.nonceStr,
+        signature: r.signature,
+        jsApiList: ['onMenuShareTimeline','onMenuShareAppMessage']
+      });
+      wx.ready(function(){
+        wx.onMenuShareTimeline({
+          title: '乐堡 | 吹气',
+          imgUrl: 'http://7xi3je.com1.z0.glb.clouddn.com/image_1471951817.437828.jpg',
+          link: 'http://www.isaac-wjl.com/yefun',
+          desc: 'WhatYouNeed | 的文章专栏与分类。'
+        });
+        wx.onMenuShareAppMessage({
+          title: '乐堡 | 吹气',
+          imgUrl: 'http://7xi3je.com1.z0.glb.clouddn.com/image_1471951817.437828.jpg',
+          link: 'http://www.isaac-wjl.com/yefun',
+          desc: 'WhatYouNeed | 的文章专栏与分类。'
+        });
+      });
+      wx.error(res => console.log(res.errMsg));
 
+
+    },
+    error: function(err) {
+      console.log(err);
+    }
+  });
 }(); 
 
   // var SHARE = [{
@@ -228,3 +259,4 @@ void function() {
   // }]
 
 });
+
